79be02a9c91a1d60439a61b880bca8f6d3494b3b
carlosEA28 <carloseaduarte28@gmail.com>
2025-02-22 15:08:00 -0300

    first route created

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,14 @@ import { fastifyCors } from "@fastify/cors";
 import {
   validatorCompiler,
   serializerCompiler,
+  ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifySwaggerUi } from "@fastify/swagger-ui";
+import { subscribeToEventRoute } from "./routes/subscribeToEventRoute";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
@@ -14,9 +19,21 @@ app.register(fastifyCors, {
   origin: true,
 });
 
-app.get("/hello", () => {
-  return "hello world";
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "NLW Connect",
+      version: "0.0.1",
+    },
+  },
+  transform: jsonSchemaTransform,
 });
+
+app.register(fastifySwaggerUi, {
+  routePrefix: "/docs",
+});
+
+app.register(subscribeToEventRoute);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running");
